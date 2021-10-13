ebebc3d5b59d082889089af4e791232b8671a7d9
Rishi Ghan <rishi.ghan@gmail.com>
2021-10-13 14:53:02 -0700

    ❌ Removed dark/light switch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,7 +57,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/rishighan/threetwo',
             label: 'GitHub',
             position: 'right',
           },
@@ -103,6 +103,9 @@ const config = {
           },
         ],
         copyright: `Copyright © ${new Date().getFullYear()} Quasar Software, LLC. Built with Docusaurus.`,
+      },
+      colorMode: {
+        disableSwitch: true,
       },
       prism: {
         theme: lightCodeTheme,
